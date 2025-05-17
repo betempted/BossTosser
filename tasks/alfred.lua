3e165f7c00b5a9d4714db6329f76c254c239192e
Leoc76101111 <a@a.a>
2025-05-17 19:13:15 +1000

    updated integration with alfred. backwards compatible with older alfred version but the old global PLUGIN_alfred_the_butler will be deprecated. updated the inventory check to use alfred's settings

--- tasks/alfred.lua
+++ tasks/alfred.lua
@@ -14,24 +14,37 @@ local task = {
 }
 
 local function reset()
-    PLUGIN_alfred_the_butler.pause(plugin_label)
+    if AlfredTheButlerPlugin then
+        AlfredTheButlerPlugin.pause(plugin_label)
+    elseif PLUGIN_alfred_the_butler then
+        PLUGIN_alfred_the_butler.pause(plugin_label)
+    end
     -- add more stuff here if you need to do something after alfred is done
     task.status = status_enum['IDLE']
 end
 
 function task.shouldExecute()
-    if settings.use_alfred and PLUGIN_alfred_the_butler then
-        local status = PLUGIN_alfred_the_butler.get_status()
-        -- add additional conditions to trigger if required
-        if (status.enabled and status.inventory_full and
-            (status.sell_count > 0 or status.salvage_count > 0)) or
-            (status.enabled and status.restock_count > 0) or
-            (status.enabled and status.teleport) or
-            (status.enabled and status.need_repair)
-        then
-            return true
-        elseif task.status == status_enum['WAITING'] then
-            return true
+    if settings.use_alfred then
+        local status = {enabled = false}
+        if AlfredTheButlerPlugin then
+            status = AlfredTheButlerPlugin.get_status()
+            -- add additional conditions to trigger if required
+            if (status.enabled and status.need_trigger) or
+                task.status == status_enum['WAITING']
+            then
+                return true
+            end
+        elseif PLUGIN_alfred_the_butler then
+            status = PLUGIN_alfred_the_butler.get_status()
+            if status.enabled and (
+                status.inventory_full or
+                status.restock_count > 0 or
+                status.need_repair or
+                status.teleport or
+                task.status == status_enum['WAITING']
+            ) then
+                return true
+            end
         end
     end
     return false
@@ -39,14 +52,22 @@ end
 
 function task.Execute()
     if task.status == status_enum['IDLE'] then
-        PLUGIN_alfred_the_butler.resume()
-        -- PLUGIN_alfred_the_butler.trigger_tasks(plugin_label,reset)
-        PLUGIN_alfred_the_butler.trigger_tasks_with_teleport(plugin_label,reset)
+        if AlfredTheButlerPlugin then
+            AlfredTheButlerPlugin.resume()
+            -- AlfredTheButlerPlugin.trigger_tasks(plugin_label,reset)
+            AlfredTheButlerPlugin.trigger_tasks_with_teleport(plugin_label,reset)
+        elseif PLUGIN_alfred_the_butler then
+            PLUGIN_alfred_the_butler.resume()
+            -- PLUGIN_alfred_the_butler.trigger_tasks(plugin_label,reset)
+            PLUGIN_alfred_the_butler.trigger_tasks_with_teleport(plugin_label,reset)
+        end
         task.status = status_enum['WAITING']
     end
 end
 
-if settings.enabled and settings.use_alfred and PLUGIN_alfred_the_butler then
+if settings.enabled and settings.use_alfred and
+    (AlfredTheButlerPlugin or PLUGIN_alfred_the_butler)
+then
     -- do an initial reset
     reset()
 end
